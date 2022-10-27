c270d7b1cb852751cdce809ce1a8d6187bade832
Arnielisado <108825315+Arnielisado@users.noreply.github.com>
2022-10-26 21:08:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function home(){
+    window.location="index.html"
+}
+function test(){
+    window.location="test.html"
+}
+function tips(){
+    window.location="tips.html"
+}
+function encuesta(){
+    window.location="encuesta.html"
+}
+function reportar(){
+    window.location="reportar.html"
+}
+function perfil(){
+    window.location="perfil.html"
+}
